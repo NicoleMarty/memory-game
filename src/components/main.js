f97746e0be2ee83d6a3eee16fd49c9c410e08e07
Nicole Marty <nmarty310@gmail.com>
2019-11-14 17:18:46 -0600

    working on dynamically generating cards so don't repeat code

--- src/components/main.js
+++ src/components/main.js
@@ -2,23 +2,78 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../style/main.css';
 
+const memoryCard =
+
+    <div className="col s12 m6 l3">
+        <div className="card">
+            <div className="card-image">
+                <img src={logo}></img>
+            </div>
+        </div>
+    </div>
+
+
+
 function main() {
     return (
         <div className="container">
             <div className="row">
                 <div className="col s12 m6 l3">
-                    <div className="card">
-                        <div className="card-image">
-
-                        </div>
-                    </div>
+                    {memoryCard}
                 </div>
-
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+            </div>
+            <div className="row">
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+            </div>
+            <div className="row">
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+            </div>
+            <div className="row">
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
+                <div className="col s12 m6 l3">
+                    {memoryCard}
+                </div>
             </div>
         </div>
-
-
-
     );
 }
 
