75e806fee8be09d671037a48b2a280c4a40e5bdd
Nicole Marty <nmarty310@gmail.com>
2020-01-05 14:18:23 -0600

    updated score and some of the text

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,27 +1,31 @@
-import React from "react";
-import "../style/score.css";
+import React from 'react'
+import '../style/score.css'
 
-function Score(props) {
-
-    return(
-        <div>
-            <div className="row top-row">
-                <div className="col s2 m2 l2">
-                    <h6 className="label">Clicky Game</h6>
-                </div>
-                <div className="col s8 m8 l8">
-                    <h6 className="clickMessage">{props.clickMessage}</h6>
-                </div>
-                <div className="col s1 m1 l1">
-                    <h6 className="correctClicks">{props.correctClicks}</h6>
-                </div>
-                <div className="col s1 m1 l1">
-                    <h6 className="topScore">{props.topScore}</h6>
-                </div>
-            </div>
+function Score (props) {
+  return (
+    <div>
+      <div className='row top-row'>
+        <div className='col s2 m2 l2'>
+          <h6 className='label'>Memory Game</h6>
         </div>
-             
-    )
+        <div className='col s8 m8 l8'>
+          <h6 className='clickMessage'>{props.clickMessage}</h6>
+        </div>
+        <div className='col s1 m1 l1'>
+          <h6 className='correctClicks'>
+            Score:<span>&nbsp;&nbsp;</span>
+            {props.correctClicks}
+          </h6>
+        </div>
+        <div className='col s1 m1 l1'>
+          <h6 className='topScore'>
+            Wins:<span>&nbsp;&nbsp;</span>
+            {props.topScore}
+          </h6>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Score;
+export default Score
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,23 +1,22 @@
-import React from "react";
-import "../style/wrapper.css";
+import React from 'react'
+import '../style/wrapper.css'
 
-
-function Wrapper(props) {
-    
+function Wrapper (props) {
   return (
-      <div>
-        <div className="row title-row">
-            <h1 className="title">Mean Girl Memory Game</h1>
-            <h5 className="directions">Careful not to click any more than once or you'll lose your chance to make them all your minions.</h5>
-            <p className="lead">(So fetch.)</p>
-           
-        </div>
-        <div class="container">
-          <div className="wrapper">{props.children}
-            </div>;
-          </div>
+    <div>
+      <div className='row title-row'>
+        <h1 className='title'>Mean Girl Memory Game</h1>
+        <h5 className='directions'>
+          Careful not to click any more than once or you'll lose your chance to
+          rule the mean girls.
+        </h5>
+        <p className='lead'>(So fetch.)</p>
       </div>
-  ) 
+      <div class='container'>
+        <div className='wrapper'>{props.children}</div>;
+      </div>
+    </div>
+  )
 }
 
-export default Wrapper;
+export default Wrapper
